c4f15c567765c8f3cc569af73e3dfedcd135bc64
9-sho-5 <hosho.kusu@icloud.com>
2022-08-04 20:13:46 +0900

    Initial Commit

---
+++ ScoreApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScoreApp
+//
+//  Created by Kusunose Hosho on 2022/08/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
